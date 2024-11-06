2ebce6504ec88b976583602f66a65055ff5c2768
BeztDonkey <vanbang9710@gmail.com>
2024-11-06 16:31:00 +0700

    untrack dashboard.db

--- src/app/components.rs
+++ src/app/components.rs
@@ -12,3 +12,12 @@ pub use toast::ToastMessageType;
 
 pub mod person_row;
 pub use person_row::PersonRow;
+
+pub mod dashboard_header;
+pub use dashboard_header::DashboardHeader;
+
+pub mod dashboard_chart;
+pub use dashboard_chart::DashboardChart;
+
+pub mod dashboard_widget;
+pub use dashboard_widget::DashboardWidget;
